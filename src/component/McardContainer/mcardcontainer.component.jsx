7f4b87561e3c2ceb118b22c2a2e667c2d5d6bfa1
Marshall Hotaling <marshall.hotaling@gmail.com>
2024-10-17 17:11:34 -0600

    made contact with the api with a async await fetch call
    we have a proper comunication with the api and the DB

--- src/component/McardContainer/mcardcontainer.component.jsx
+++ src/component/McardContainer/mcardcontainer.component.jsx
@@ -1,12 +1,31 @@
 import './mcardcontainer.css'
-import moviePosters from '../../data/movie_posters'
 import Mcard from '../Mcard/mcard.component'
 import { Fragment } from 'react'
+import {useState} from "react";
+import {useEffect} from "react";
 
 const McardContainer = ({pageEventHandler,mIdSetter}) => {
+  const [moviesData,setMoviesData ] = useState([]);
+
+  useEffect (()=>{
+
+    const fetchData = async ()=> {
+      try {
+        const moviesResponse = await fetch(
+          `https://rancid-tomatillos-api-cc6f59111a05.herokuapp.com/api/v1/movies`
+        )
+        const movies = await moviesResponse.json()
+        setMoviesData([...moviesData, ...movies])
+      } catch (err) {
+        console.log(err, "err")
+      }
+    }
+    fetchData()
+  },[])
 
   const movieCard = () => {
-    return moviePosters.map((data) => {
+
+    return moviesData.map((data) => {
       return (
         <Mcard 
           key={data.id}
