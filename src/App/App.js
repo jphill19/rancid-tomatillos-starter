bc5a47f965c10a018a05107e1322b91d2886f6e3
Marshall Hotaling <marshall.hotaling@gmail.com>
2024-10-23 17:59:50 -0600

    refactord cypress testing
    implumented cypress for the new react router functinaluty

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { Route,Routes} from "react-router-dom";
 import  Home from '../routes/home.component'
-import McardContainer from "../component/McardContainer/mcardcontainer.component";
 import Mdetails from "../component/Mdetails/mdetails.component";
 
 function App() {
@@ -11,9 +10,7 @@ function App() {
       <Route path="/" element={<Home/>}>
         <Route path=":movieId" element={<Mdetails/>}/>
         <Route path="*" element={<h2>Cannot find anything under that route</h2>}/>
-
       </Route>
-    
     </Routes>
 
 
